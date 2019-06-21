d65b915046e34e323f730101dbb9fcdb9f5a162f
幻想不要钱 <huijia0211@gmail.com>
2019-06-21 18:14:03 +0800

    增加分类管理控制类

---
+++ src/main/java/com/mmall/controller/backend/CategoryManagerController.java
@@ -0,0 +1,23 @@
+package com.mmall.controller.backend;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Admin
+ */
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManagerController {
+
+    public ServerResponse addCategory(HttpSession session, String categoryName, @RequestParam(value = "parentId", defaultValue = "0") int parentId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        return null;
+    }
+}
