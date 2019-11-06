0344cf5ba06c1d038aa74906d85664b67b4046f7
huijia <huijia0211@gmail.com>
2019-11-07 01:13:28 +0800

    购物车

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -17,6 +17,15 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc", "price_asc");
     }
 
+    public interface Cart{
+        //购物车选中状态
+        int CHECKED = 1;
+        //购物车未选中状态
+        int UN_CHECK = 0;
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+    }
+
     public interface Role {
         /**
          * 普通用户
