44f4b2f96991dd350d143da3580a262a312a7ec1
huijia <huijia0211@gmail.com>
2019-11-16 23:52:11 +0800

    支付宝支付和回调

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -17,7 +17,7 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc", "price_asc");
     }
 
-    public interface Cart{
+    public interface Cart {
         //购物车选中状态
         int CHECKED = 1;
         //购物车未选中状态
@@ -49,6 +49,63 @@ public class Const {
         private int code;
 
         ProductStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public enum OrderStatusEnum {
+        /**
+         * 已取消
+         */
+        CANCELED(0, "已取消"),
+        NO_PAY(10, "未支付"),
+        PAID(20, "已付款"),
+        SHIPPED(40, "已发货"),
+        ORDER_SUCCESS(50, "订单完成"),
+        ORDER_CLOSE(60, "订单关闭");
+        private String value;
+        private int code;
+
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public interface AlipayCallback {
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+    public enum PayPlatFormEnum {
+        /**
+         * 已取消
+         */
+        ALIPAY(1, "支付宝");
+        private String value;
+        private int code;
+
+        PayPlatFormEnum(int code, String value) {
             this.code = code;
             this.value = value;
         }
