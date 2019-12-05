efcbfb6f0cdc4b19f0dd4b942052df113ae22e42
huijia <huijia0211@gmail.com>
2019-12-05 23:27:50 +0800

    guava迁移redis

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -2,11 +2,11 @@ package com.mmall.service.impl;
 
 import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
-import com.mmall.common.TokenCache;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import com.mmall.util.MD5Util;
+import com.mmall.util.RedisPoolUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -126,7 +126,8 @@ public class UserServiceImpl implements IUserService {
             //使用UUID生成token
             String forgetToken = UUID.randomUUID().toString();
             //将token加入缓存，key为TokenCache.TOKEN_PREFIX
-            TokenCache.setKey(TokenCache.TOKEN_PREFIX + username, forgetToken);
+//            TokenCache.setKey(TokenCache.TOKEN_PREFIX + username, forgetToken);
+            RedisPoolUtil.setEx(Const.TOKEN_PREFIX + username, forgetToken, 60*60*12);
             //将token返给前端
             return ServerResponse.createBySuccess(forgetToken);
         }
@@ -146,7 +147,8 @@ public class UserServiceImpl implements IUserService {
             return ServerResponse.createByErrorMessage("用户不存在");
         }
         //根据key从缓存获取token
-        String token = TokenCache.getKey(TokenCache.TOKEN_PREFIX + username);
+//        String token = TokenCache.getKey(TokenCache.TOKEN_PREFIX + username);
+        String token = RedisPoolUtil.get(Const.TOKEN_PREFIX + username);
         //判断是否获取到token
         if (StringUtils.isBlank(token)) {
             return ServerResponse.createByErrorMessage("token无效或者过期");
