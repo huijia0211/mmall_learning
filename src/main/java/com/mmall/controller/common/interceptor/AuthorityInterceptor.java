ce2d67b4ac0f002e8fe79e4f27d8afa75819e88e
huijia <huijia0211@gmail.com>
2019-12-11 00:39:21 +0800

    全局异常和拦截器配置

---
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,30 @@
+package com.mmall.controller.common.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Admin
+ */
+@Slf4j
+public class AuthorityInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        log.info("preHandle");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        log.info("afterCompletion");
+    }
+}
